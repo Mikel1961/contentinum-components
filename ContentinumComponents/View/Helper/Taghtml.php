27e6d576ff0174ffd28a504959fd048fbee15c55
mikel1961 <michael.jochum@jochum-mediaservices.de>
2013-08-14 10:30:49 +0200

    Add ViewHelpers for content treatment
    Change-Id: I5c332a565656e8d0a38be965d151559ceda99f0d

---
+++ ContentinumComponents/View/Helper/Taghtml.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * contentinum - accessibility websites
+ *
+ * LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category contentinum components
+ * @package View
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ * @copyright Copyright (c) 2009-2013 jochum-mediaservices, Katja Jochum (http://www.jochum-mediaservices.de)
+ * @license http://www.opensource.org/licenses/bsd-license
+ * @since contentinum version 5.0
+ * @link      https://github.com/Mikel1961/contentinum-components
+ * @version   1.0.0
+ */
+namespace ContentinumComponents\View\Helper;
+
+use Zend\Form\View\Helper\AbstractHelper;
+use ContentinumComponents\Html\HtmlAttribute;
+
+/**
+ * Set content in html tag
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ */
+class Taghtml extends AbstractHelper
+{
+
+	public function __invoke($content, $tag, $attributes = null, $additional = null)
+	{
+        $html = '';
+        if (strlen($tag) >= 1) {
+            $html .= '<' . $tag . '';
+            
+            if ( is_array($attributes) && ! empty($attributes) ){
+            	$html .= HtmlAttribute::attributeArray($attributes);
+            }
+            $html .= '>';
+        }
+        $html .= $content . $additional;
+        if (strlen($tag) >= 1) {
+            $html .= '</' . $tag . '>';
+        }
+        
+        return $html;       
+	}
+
+}
