82a54ffc0b9dae4445810e6e0dd999044d77835e
mikel1961 <michael.jochum@jochum-mediaservices.de>
2013-09-30 16:12:02 +0200

    Revision and add further methods
    Change-Id: If4259a0507a84bf512aebb6d34d7d04189d61fa1

--- ContentinumComponents/Storage/StorageFiles.php
+++ ContentinumComponents/Storage/StorageFiles.php
@@ -36,6 +36,7 @@ use ContentinumComponents\Storage\Exeption\InvalidValueStorageException;
  */
 class StorageFiles extends AbstractStorage
 {
+	const ERROR_FILE_EXIST = 'The file does not exist or it was a false name or place specified';
 	const ERROR_READ_FILE = 'Can not open or read the file, missing filename!';
 	const ERROR_METHOD_READ_FILE = 'No method to read file content define!';
 	
@@ -68,9 +69,8 @@ class StorageFiles extends AbstractStorage
 		return $this;
 	}	
 	
-
 	/**
-	 * 
+	 * Warpper method for file_get_contents
 	 * @param string $path
 	 * @param string $file
 	 * @throws InvalidValueStorageException
@@ -78,29 +78,32 @@ class StorageFiles extends AbstractStorage
 	 */
 	public function fetchFileContent($path = null,$file = null)
 	{
-		if (!$path){
-			if (method_exists($this->getEntity(), 'getCurrentPath')){
-				$path = $this->getEntity()->getCurrentPath();
+		$path = $this->issetPath($path);
+		$file = $this->issetFile($file);
+		$dir = $this->getStorage()->setPath($path)->setCurrent($file)->getAdapter ();
+		
+		if ( ! is_file($dir) ){
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_FILE_EXIST);
 			}
-			if (!$path){
-				throw new InvalidValueStorageException('No directory path given');
-			} 
+			throw new InvalidValueStorageException(self::ERROR_FILE_EXIST);
 		}
 		
-		if (!$file){
-			if (null == ($file = $this->getFile())){
-				throw new InvalidValueStorageException('No file name given');
-			}
-		}
-		
-		if (false != ($content = @file_get_contents($this->getStorage()->setPath($path)->setCurrent($file)->getAdapter ()))){
+		if (false !== ($content = @file_get_contents($dir))){
+			if (true == ($log = $this->getLogger())){
+				$log->info('Read content from file '. $dir);
+			}			
 			return $content;
+		} else {
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_READ_FILE);
+			}			
+			throw new InvalidValueStorageException(self::ERROR_READ_FILE);
 		}
-		return false;
 	}
 	
 	/**
-	 * 
+	 * Warpper method for file_put_contents
 	 * @param string $data file content data
 	 * @param string $path
 	 * @param string $file
@@ -109,6 +112,73 @@ class StorageFiles extends AbstractStorage
 	 */
 	public function setFileContent($data, $path = null,$file = null)
 	{
+		$path = $this->issetPath($path);
+		$file = $this->issetFile($file);
+		$dir = $this->getStorage()->setPath($path)->setCurrent($file)->getAdapter ();
+		
+		if ( ! is_file($dir) ){
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_FILE_EXIST);
+			}			
+			throw new InvalidValueStorageException(self::ERROR_FILE_EXIST);
+		}
+		
+		if ( false === @file_put_contents($dir, $data) ){
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_READ_FILE);
+			}
+			throw new InvalidValueStorageException(self::ERROR_READ_FILE);
+		} else {
+			if (true == ($log = $this->getLogger())){
+				$log->info('Write content in file '. $dir);
+			}			
+			return true;
+		}
+		
+	}
+	
+	/**
+	 * Copy a file
+	 * @param string $dest destination path and file
+	 * @param string $path source path
+	 * @param string $file source file
+	 * @throws InvalidValueStorageException
+	 * @return boolean
+	 */
+	public function copyFile($dest, $path = null,$file = null)
+	{
+		$path = $this->issetPath($path);
+		$file = $this->issetFile($file);
+		$dir = $this->getStorage()->setPath($path)->setCurrent($file)->getAdapter ();		
+
+		if ( ! is_file($dir) ){
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_FILE_EXIST);
+			}
+			throw new InvalidValueStorageException(self::ERROR_FILE_EXIST);
+		}
+
+		if (! copy($dir, $dest)){
+			if (true == ($log = $this->getLogger())){
+				$log->err(self::ERROR_READ_FILE);
+			}
+			throw new InvalidValueStorageException(self::ERROR_READ_FILE);			
+		} else {
+			if (true == ($log = $this->getLogger())){
+				$log->info('Copy file '. $dir . ' to ' . $dest);
+			}			
+			return true;
+		}
+	}
+	
+	/**
+	 * Check if a path has been specified, or if it can be determined
+	 * @param string $path
+	 * @throws InvalidValueStorageException
+	 * @return string
+	 */
+	protected function issetPath($path = null)
+	{
 		if (!$path){
 			if (method_exists($this->getEntity(), 'getCurrentPath')){
 				$path = $this->getEntity()->getCurrentPath();
@@ -117,18 +187,23 @@ class StorageFiles extends AbstractStorage
 				throw new InvalidValueStorageException('No directory path given');
 			}
 		}
-		
+		return $path;		
+	}
+	
+	/**
+	 * check if a file name has been specified, or if it can be determined
+	 * @param string $file
+	 * @throws InvalidValueStorageException
+	 * @return string
+	 */
+	protected function issetFile($file = null)
+	{
 		if (!$file){
 			if (null == ($file = $this->getFile())){
 				throw new InvalidValueStorageException('No file name given');
 			}
 		}
-
-		if ( file_put_contents($this->getStorage()->setPath($path)->setCurrent($file)->getAdapter (), $data) ){
-			return true;
-		} else {
-			return false;
-		}
-		
+		return $file;		
 	}
+	
 }
