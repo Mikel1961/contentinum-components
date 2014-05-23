babd6552ff047b1a34f29ad2bcf9890b9f9f3c22
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2014-05-23 15:02:43 +0200

    Add configuration elements show outside fieldset

--- ContentinumComponents/View/Helper/RenderForm.php
+++ ContentinumComponents/View/Helper/RenderForm.php
@@ -32,240 +32,248 @@ use ContentinumComponents\Html\HtmlAttribute;
 
 /**
  * Build and create a html form
+ * 
  * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
- *
+ *        
  */
 class RenderForm extends AbstractHelper
 {
 
-	/**
-	 * Build, create form
-	 * @param unknown $form
-	 * @return string html form
-	 */
-	public function __invoke($form)
-	{
-		$form->prepare();
-		$html = $this->view->form()->openTag($form);
-		$html .= $this->renderFieldsets($form->getFieldsets());
-		$html .= $this->renderElements($form->getElements());
-		$html .= $this->view->form()->closeTag($form);
-		return $html;
-	}	
-	
-	/**
-	 * Render a array of field sets
-	 * @param array $fieldsets
-	 * @return string
-	 */
-	private function renderFieldsets($fieldsets)
-	{
-		$html = '';
-	
-		foreach($fieldsets as $fieldset)
-		{
-			if(count($fieldset->getFieldsets()) > 0) {
-				$html .= $this->renderFieldsets($fieldset->getFieldsets());
-			}
-	
-			$html .= $this->renderElements($fieldset->getElements());
-		}
-	
-		return $html;
-	}
-	
-	/**
-	 * Render a array from form field elements
-	 * @param array $elements array from form field elements
-	 * @return string
-	 */
-	private function renderElements($elements)
-	{
-		$html = '';
-		foreach($elements as $element) {
-			$tmp[] = $this->renderElement($element);
-			if (true == ($fieldset = $element->getOption('fieldset'))) {
-			    $attributes = '';
-			    if (isset($fieldset['attributes'])){
-			        $attributes = HtmlAttribute::attributeArray($fieldset['attributes']);
-			    }
-			    $html .= '<fieldset'. $attributes . '>';
-			    if (isset($fieldset['legend'])){
-			        $html .= '<legend>' . $fieldset['legend'] . '</legend>';
-			    }
-			    foreach ($tmp as $element){
-			        $html .= $element;
-			    }
-			    $html .= '</fieldset>';
-			    $tmp = array();
-			}
-		}
-		foreach ($tmp as $element){
-		        $html .= $element;
-		}
-		return $html;
-	}
-	
-	/**
-	 * 
-	 * Render a form field row
-	 * 
-	 */
-	private function renderElement($element)
-	{
-		$html = '';
-		$type = $element->getAttribute('type');
-		$formLabel = $this->view->plugin('formLabel');
-		if ( $element->getOption('label') ){
-			$html .= $formLabel->openTag();
-			$html .= $element->getOption('label');
-			$html .= $formLabel->closeTag();
-		}
-		
-		$html .= $this->view->formElement($element);
-		if( $type == 'submit') {
-			$abortDeco = $element->getOption('deco-abort-btn');
-			$label = 'Button';
-			if ( isset($abortDeco['label']) ){
-				$label = $abortDeco['label'];
-			}
-			if ($this->view->abortation){
-				$html .= $this->renderButton($label,$abortDeco);
-			}
-		}
-		
-		$html .= $this->renderErrors($element);
-		$html .= $this->renderDescription($element->getOption('description'));
-		
-		if (true == ($deco = $element->getOption('deco-row'))) {
-		    if (isset($deco['tags'])){
-		        $html = $this->buildDecco($deco, $html);
-		    } else {
-    			$attributes = '';
-    			if ( isset($deco['attributes'])){
-    				$attributes = HtmlAttribute::attributeArray($deco['attributes']);
-    			}			
-    			$html = '<' . $deco['tag'] . '' . $attributes .'>' . $html .'</' . $deco['tag'] . '>'; 
-		    } 
-			
-		}
-		
-		return $html;
-		
-	}	
-	
-	private function buildDecco($deco, $html)
-	{
-	    $start = $end = '';
-	    foreach ($deco['tags'] as $element){
-	        $attributes = '';
-	        if ( isset($element['attributes'])){
-	        	$attributes = HtmlAttribute::attributeArray($element['attributes']);
-	        }	        
-	        
-	        $start .= '<' . $element['tag'] . $attributes . '>';
-	        $end .= '</' . $element['tag'] . '>'; 
-	    }
-	    return $start . $html . $end;
-	}
+    /**
+     * Build, create form
+     * 
+     * @param unknown $form            
+     * @return string html form
+     */
+    public function __invoke($form)
+    {
+        $form->prepare();
+        $html = $this->view->form()->openTag($form);
+        $html .= $this->renderFieldsets($form->getFieldsets());
+        $html .= $this->renderElements($form->getElements());
+        $html .= $this->view->form()->closeTag($form);
+        return $html;
+    }
 
-	
-	/**
-	 * Build form field error messages
-	 * @param FormElement $element
-	 * @return string
-	 */
-	private function renderErrors($element)
-	{
-		$html = false;
-		$tag = false;
-		$attributes = false;
-		if ($this->view->formElementErrors()){
-		    $tag = 'span';
-			$err = $this->view->formElementErrors();
-			$err->setMessageOpenFormat('<span class="server-error" role="alert">');
-			$err->setMessageCloseString('</span>');
-			$err->setMessageSeparatorString('<br />');
-			$html = $err->render($element); 
-		} 
-		
-		
-		if (false == $html){
-		    if (true == ($msg = $element->getOption('deco-error-msg'))  ){
-		        if (true == ($deco = $element->getOption('deco-error'))) {
-		            if ( isset($deco['tag']) ){
-		            	$tag = $deco['tag'];
-		            }
-		            if ( isset($deco['attributes'])){
-		            	$attributes = HtmlAttribute::attributeArray($deco['attributes']);
-		            }
-		            $html = '<'.$tag.''.$attributes. '>' . $msg . '</' . $tag . '>'; 		            
-		        }
-		    }
-		}
-		return $html;
+    /**
+     * Render a array of field sets
+     * 
+     * @param array $fieldsets            
+     * @return string
+     */
+    private function renderFieldsets($fieldsets)
+    {
+        $html = '';
+        
+        foreach ($fieldsets as $fieldset) {
+            if (count($fieldset->getFieldsets()) > 0) {
+                $html .= $this->renderFieldsets($fieldset->getFieldsets());
+            }
+            
+            $html .= $this->renderElements($fieldset->getElements());
+        }
+        
+        return $html;
+    }
 
-	}
-	
-	/**
-	 * Build field description
-	 * @param array $desc
-	 * @return string
-	 */
-	private function renderDescription($desc, $key = 'deco-desc')
-	{
-		$html = '';
-		if ( is_array($desc) && !empty($desc) ){
-			
-			$tag = 'span';
-			$attributes = '';
-			if (true == ($deco = $desc[$key])) {
-				if ( isset($deco['tag']) ){
-					$tag = $deco['tag'];
-				}
-				if ( isset($deco['attributes'])){
-					$attributes = HtmlAttribute::attributeArray($deco['attributes']);
-				}
-			}
-			
-			$html = '<' . $tag . '' . $attributes . '>' . $desc['text'] . '</' . $tag . '>';
-		}
-		return $html;		
-	}
-	
-	/**
-	 * Build and create a button
-	 * @param string $label button label
-	 * @param array $decorator button attributtes and row tag
-	 * @return string
-	 */
-	private function renderButton($label, $decorator = null)
-	{
-		$attributtes = '';
-		$html = '';
-		$endTag = '';
-		if ( $decorator ){
-			if (isset($decorator['attributes']) && is_array($decorator['attributes'])){
-				$attributtes = HtmlAttribute::attributeArray($decorator['attributes']);
-			}
-				
-			if (isset($decorator['tag'])){
-				$html .= '<'. $decorator['tag'];
-			}
-				
-			if (isset($decorator['tag-attribs']) && is_array($decorator['tag-attribs'])){
-				$html .= HtmlAttribute::attributeArray($decorator['tag-attribs']);
-			}
-				
-			if (isset($decorator['tag'])){
-				$html .= '>';
-				$endTag = '</'. $decorator['tag'] . '>';
-			}
-		}
-		$html .= '<a' . $attributtes;
-		$html .= ' href="' . $this->view->abortation . '">';
-		$html .= $label . '</a>' . $endTag;
-		return $html;
-	}	
+    /**
+     * Render a array from form field elements
+     * 
+     * @param array $elements
+     *            array from form field elements
+     * @return string
+     */
+    private function renderElements($elements)
+    {
+        $html = '';
+        foreach ($elements as $element) {
+            $tmp[] = $this->renderElement($element);
+            if (true == ($fieldset = $element->getOption('fieldset'))) {
+                if (isset($fieldset['nofieldset'])) {
+                    foreach ($tmp as $element) {
+                        $html .= $element;
+                    }
+                    $tmp = array();
+                } else {
+                    $attributes = '';
+                    if (isset($fieldset['attributes'])) {
+                        $attributes = HtmlAttribute::attributeArray($fieldset['attributes']);
+                    }
+                    $html .= '<fieldset' . $attributes . '>';
+                    if (isset($fieldset['legend'])) {
+                        $html .= '<legend>' . $fieldset['legend'] . '</legend>';
+                    }
+                    foreach ($tmp as $element) {
+                        $html .= $element;
+                    }
+                    $html .= '</fieldset>';
+                    $tmp = array();
+                }
+            }
+        }
+        foreach ($tmp as $element) {
+            $html .= $element;
+        }
+        return $html;
+    }
 
+    /**
+     * Render a form field row
+     */
+    private function renderElement($element)
+    {
+        $html = '';
+        $type = $element->getAttribute('type');
+        $formLabel = $this->view->plugin('formLabel');
+        if ($element->getOption('label')) {
+            $html .= $formLabel->openTag();
+            $html .= $element->getOption('label');
+            $html .= $formLabel->closeTag();
+        }
+        
+        $html .= $this->view->formElement($element);
+        if ($type == 'submit') {
+            $abortDeco = $element->getOption('deco-abort-btn');
+            $label = 'Button';
+            if (isset($abortDeco['label'])) {
+                $label = $abortDeco['label'];
+            }
+            if ($this->view->abortation) {
+                $html .= $this->renderButton($label, $abortDeco);
+            }
+        }
+        
+        $html .= $this->renderErrors($element);
+        $html .= $this->renderDescription($element->getOption('description'));
+        
+        if (true == ($deco = $element->getOption('deco-row'))) {
+            if (isset($deco['tags'])) {
+                $html = $this->buildDecco($deco, $html);
+            } else {
+                $attributes = '';
+                if (isset($deco['attributes'])) {
+                    $attributes = HtmlAttribute::attributeArray($deco['attributes']);
+                }
+                $html = '<' . $deco['tag'] . '' . $attributes . '>' . $html . '</' . $deco['tag'] . '>';
+            }
+        }
+        
+        return $html;
+    }
+
+    private function buildDecco($deco, $html)
+    {
+        $start = $end = '';
+        foreach ($deco['tags'] as $element) {
+            $attributes = '';
+            if (isset($element['attributes'])) {
+                $attributes = HtmlAttribute::attributeArray($element['attributes']);
+            }
+            
+            $start .= '<' . $element['tag'] . $attributes . '>';
+            $end .= '</' . $element['tag'] . '>';
+        }
+        return $start . $html . $end;
+    }
+
+    /**
+     * Build form field error messages
+     * 
+     * @param FormElement $element            
+     * @return string
+     */
+    private function renderErrors($element)
+    {
+        $html = false;
+        $tag = false;
+        $attributes = false;
+        if ($this->view->formElementErrors()) {
+            $tag = 'span';
+            $err = $this->view->formElementErrors();
+            $err->setMessageOpenFormat('<span class="server-error" role="alert">');
+            $err->setMessageCloseString('</span>');
+            $err->setMessageSeparatorString('<br />');
+            $html = $err->render($element);
+        }
+        
+        if (false == $html) {
+            if (true == ($msg = $element->getOption('deco-error-msg'))) {
+                if (true == ($deco = $element->getOption('deco-error'))) {
+                    if (isset($deco['tag'])) {
+                        $tag = $deco['tag'];
+                    }
+                    if (isset($deco['attributes'])) {
+                        $attributes = HtmlAttribute::attributeArray($deco['attributes']);
+                    }
+                    $html = '<' . $tag . '' . $attributes . '>' . $msg . '</' . $tag . '>';
+                }
+            }
+        }
+        return $html;
+    }
+
+    /**
+     * Build field description
+     * 
+     * @param array $desc            
+     * @return string
+     */
+    private function renderDescription($desc, $key = 'deco-desc')
+    {
+        $html = '';
+        if (is_array($desc) && ! empty($desc)) {
+            
+            $tag = 'span';
+            $attributes = '';
+            if (true == ($deco = $desc[$key])) {
+                if (isset($deco['tag'])) {
+                    $tag = $deco['tag'];
+                }
+                if (isset($deco['attributes'])) {
+                    $attributes = HtmlAttribute::attributeArray($deco['attributes']);
+                }
+            }
+            
+            $html = '<' . $tag . '' . $attributes . '>' . $desc['text'] . '</' . $tag . '>';
+        }
+        return $html;
+    }
+
+    /**
+     * Build and create a button
+     * 
+     * @param string $label
+     *            button label
+     * @param array $decorator
+     *            button attributtes and row tag
+     * @return string
+     */
+    private function renderButton($label, $decorator = null)
+    {
+        $attributtes = '';
+        $html = '';
+        $endTag = '';
+        if ($decorator) {
+            if (isset($decorator['attributes']) && is_array($decorator['attributes'])) {
+                $attributtes = HtmlAttribute::attributeArray($decorator['attributes']);
+            }
+            
+            if (isset($decorator['tag'])) {
+                $html .= '<' . $decorator['tag'];
+            }
+            
+            if (isset($decorator['tag-attribs']) && is_array($decorator['tag-attribs'])) {
+                $html .= HtmlAttribute::attributeArray($decorator['tag-attribs']);
+            }
+            
+            if (isset($decorator['tag'])) {
+                $html .= '>';
+                $endTag = '</' . $decorator['tag'] . '>';
+            }
+        }
+        $html .= '<a' . $attributtes;
+        $html .= ' href="' . $this->view->abortation . '">';
+        $html .= $label . '</a>' . $endTag;
+        return $html;
+    }
 }
