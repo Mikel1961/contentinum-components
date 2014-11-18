4e1922989fdf39d87655c407b22380a72ee2d3df
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2014-11-18 09:12:21 +0100

    View Helper news, add single view for a news messages

--- ContentinumComponents/View/Helper/Content/News.php
+++ ContentinumComponents/View/Helper/Content/News.php
@@ -29,41 +29,221 @@ namespace ContentinumComponents\View\Helper\Content;
 
 use Zend\View\Helper\AbstractHelper;
 
-
 class News extends AbstractHelper
 {
+
+    /**
+     *
+     * @var unknown
+     */
+    private $row;
+
+    /**
+     *
+     * @var unknown
+     */
+    private $grid;
+
+    /**
+     *
+     * @var unknown
+     */
+    private $header;
+
+    /**
+     *
+     * @var unknown
+     */
+    private $footer;
+
+    /**
+     *
+     * @var unknown
+     */
+    private $publishAuthor;
+
+    /**
+     *
+     * @var unknown
+     */
+    private $labelReadMore;
+
+    private $properties = array(
+        'row',
+        'grid',
+        'header',
+        'footer',
+        'publishAuthor',
+        'labelReadMore'
+    );
+
     public function __invoke(array $content, $medias, $template = null, $teasers = true)
     {
+        $this->setTemplate($template);
+        
+        $header = $this->getTemplateProperty('header', 'row');
+        $footer = $this->getTemplateProperty('footer', 'row');
+        
+        $newselement = $this->getTemplateProperty('row', 'element');
+        $grid = $this->getTemplateProperty('grid', 'element');
+        $cut = true;
+        if (false !== $this->view->article && $content['entries'][$this->view->article] ){
+            $content['entries'] = array($content['entries'][$this->view->article]);
+            $cut = false; 
+        }
+        
         $html = '';
         foreach ($content['entries'] as $row) {
-            $html .= '<h2>' . $row['headline'] . '</h2>';
-            if (strlen($row['contentTeaser']) > 1){
-                $html .= $row['contentTeaser'];
-            } else {
-                $content = $row['content'];
-                if (strlen($row['numberCharacterTeaser']) > 0){
-                    $content = substr($content, 0, $row['numberCharacterTeaser']);
-                    $content = substr($content, 0, strrpos($content, " "));
-                    $content = $content . ' ...</p>';                    
+            $newsrow = '';
+            if ('1' !== $row['id']) {
+                $head = '<time>' . $this->view->dateFormat(new \DateTime($row['publishDate']), \IntlDateFormatter::FULL) . '</time>';
+                $head .= '<h2>' . $row['headline'] . '</h2>';
+                $attr = array();
+                if ($header) {
+                    if (isset($header['attr'])) {
+                        $attr = $header['attr'];
+                    }
+                    $newsrow .= $this->view->contentelement($header['element'], $head, $attr);
+                    $attr = array();
+                } else {
+                    $newsrow .= $head;
                 }
-                $html .= $content;
-            }
-            $footer = '';
-            if (strlen($row['publishAuthor']) > 1 ){
-                $footer .= '<p><span>' . $row['publishAuthor'] . '</span>';
-            }
-            
-            if (strlen($row['publishDate']) > 1){
-                if (strlen($footer) == 0){
-                    $footer .= '<p>';
+                if (true === $cut){
+                    if (strlen($row['contentTeaser']) > 1) {
+                        $newsrow .= $row['contentTeaser'];
+                        $newsrow .= $this->readMoreLink($row);
+                    } else {
+                        $content = $row['content'];
+                        if (strlen($row['numberCharacterTeaser']) > 0) {
+                            $content = substr($content, 0, $row['numberCharacterTeaser']);
+                            $content = substr($content, 0, strrpos($content, " "));
+                            $content = $content . ' ...</p>';
+                        }
+                        $newsrow .= $content;
+                        $newsrow .= $this->readMoreLink($row);
+                    }
+                } else {
+                    $newsrow .= $row['contentTeaser'];
+                    $newsrow .= $row['content'];
                 }
-                $footer .= '<span>' . $row['publishDate'] . '</span>';
+                
+                
+                if ($footer) {
+                    if (isset($footer['attr'])) {
+                        $attr = $footer['attr'];
+                    }
+                    $newsrow .= $this->view->contentelement($footer['element'], $this->formatPublishAuthor($row), $attr);
+                    $attr = array();
+                } else {
+                    $newsrow .= $this->formatPublishAuthor($row);
+                }                
+                $attr = array();
+                if ($grid){
+                    if (false !== $this->getTemplateProperty('grid', 'attr')){
+                        $attr = $this->getTemplateProperty('grid', 'attr');
+                    }
+                    $html .= $this->view->contentelement($grid, $newsrow, $attr );
+                } else {
+                    $html .= $newsrow;
+                }
+                
             }
-            if (strlen($footer) > 1){
-                $footer .= '</p>';
-            }   
-            $html .= $footer;         
+        }
+        
+        if (false !== $newselement){
+            $html = $this->view->contentelement($newselement, $html, $this->getTemplateProperty('row', 'attr'));
         }
         return $html;
+    }
+
+    /**
+     *
+     * @param unknown $prop
+     * @param unknown $key
+     * @return boolean
+     */
+    protected function getTemplateProperty($prop, $key)
+    {
+        if (isset($this->{$prop}[$key])) {
+            return $this->{$prop}[$key];
+        } else {
+            return false;
+        }
+    }
+
+    protected function setTemplate($template)
+    {
+            foreach ($template as $key => $values) {
+                if (in_array($key, $this->properties)) {
+                    $this->{$key} = $values;
+                }
+            }
+    }
+
+    /**
+     *
+     * @param unknown $row
+     * @return unknown
+     */
+    protected function formatPublishAuthor($row)
+    {
+        if (strlen($row['publishAuthor']) > 1) {
+            if ($this->publishAuthor) {
+                $elm = $this->getTemplateProperty('publishAuthor', 'row');
+                $grid = $this->getTemplateProperty('publishAuthor', 'grid');
+                $publishAuthor = $row['publishAuthor'];
+                if (isset($grid['element'])) {
+                    if (isset($grid['attr'])) {
+                        $attr = $grid['attr'];
+                    }
+                    $publishAuthor = $this->view->contentelement($grid['element'], $publishAuthor, $attr);
+                    $attr = array();
+                }
+                if (isset($elm['attr'])) {
+                    $attr = $elm['attr'];
+                }
+                $publishAuthor = $this->view->contentelement($elm['element'], $publishAuthor, $attr);
+            } else {
+                $publishAuthor = $row['publishAuthor'];
+            }
+            return $publishAuthor;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     *
+     * @param unknown $row
+     * @return string
+     */
+    protected function readMoreLink($row)
+    {
+        if (strlen($row['labelReadMore']) > 1) {
+            if ($this->labelReadMore) {
+                $elm = $this->getTemplateProperty('labelReadMore', 'row');
+                $grid = $this->getTemplateProperty('labelReadMore', 'grid');
+                $attr['href'] = $this->view->pageurl . '/' . $row['source'];
+                $attr['title'] = $row['labelReadMore'] . ' ' . $row['headline'];
+                $readMore = $row['labelReadMore'];
+                if (isset($grid['element'])) {
+                    if (isset($grid['attr'])) {
+                        $attr = array_merge($grid['attr'], $attr);
+                    }
+                    $readMore = $this->view->contentelement($grid['element'], $readMore, $attr);
+                    $attr = array();
+                }
+                if (isset($elm['attr'])) {
+                    $attr = $elm['attr'];
+                }
+                $readMore = $this->view->contentelement($elm['element'], $readMore, $attr);
+            } else {
+                $readMore = '<a href="/' . $row['source'] . '" title="' . $row['labelReadMore'] . ' ' . $row['headline'] . '">';
+                $readMore .= $row['labelReadMore'] . '</a>';
+            }
+            return $readMore;
+        } else {
+            return '';
+        }
     }
 }
