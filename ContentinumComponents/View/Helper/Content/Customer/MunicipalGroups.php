a17bcf1b45a56b240399300f12cbd52e65905b39
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2015-01-12 17:39:21 +0100

    Configure the view and microdatas

--- ContentinumComponents/View/Helper/Content/Customer/MunicipalGroups.php
+++ ContentinumComponents/View/Helper/Content/Customer/MunicipalGroups.php
@@ -27,37 +27,122 @@
  */
 namespace ContentinumComponents\View\Helper\Content\Customer;
 
-
 use ContentinumComponents\View\Helper\Content\AbstractContentHelper;
+
 class MunicipalGroups extends AbstractContentHelper
 {
+
     /**
      *
-     * @var unknown
+     * @var array
      */
-    private $row;
-    
+    protected $row;
+
     /**
      *
-     * @var unknown
-    */
-    private $grid;
-    
+     * @var array
+     */
+    protected $grid;
+
     /**
      *
-     * @var unknown
-    */
-    private $properties = array(
+     * @var array
+     */
+    protected $elements;
+
+    /**
+     *
+     * @var array
+     */
+    protected $properties = array(
         'row',
         'grid',
+        'elements'
+    )
+    ;
+
+    protected $salutation = array(
+        'mr' => 'Herr',
+        'ms' => 'Frau'
     );
 
     public function __invoke(array $entries, $medias, $template)
     {
+        $departments = $entries['modulContent']['departments'];
+        $business = $entries['modulContent']['business'];
+        $id = 0;
         $html = '';
-        foreach ($entries['modulContent'] as $entry){
-            $html .= '<p>' . $entry->contact->firstName . ' ' . $entry->contact->lastName . '</p>';
+        $filter = new \Zend\Filter\Digits(); // business
+        foreach ($departments as $entry) {
+            if ('4' != $entry->employeeTypes->id) {
+                $html .= $this->buildContact($entry, $entry->departments->name, $filter);
+            }
+            if ($id !== $entry->departments->id) {
+                if (isset($business[$entry->departments->id])) {
+                    $html .= $this->buildBusiness($business[$entry->departments->id]);
+                }
+            }
+            $id = $entry->departments->id;
         }
         return $html;
+    }
+
+    protected function buildBusiness($businessEntries)
+    {
+        $html = '';
+        $filter = new \Zend\Filter\Digits(); // business
+        foreach ($businessEntries as $entry) {
+            if ('4' != $entry->employeeTypes->id) {
+                $html .= $this->buildContact($entry, $entry->business->name . '<br />' . $entry->business->nameDesc, $filter);
+            }
+        }
+        return $html;
+    }
+
+    /**
+     *
+     * @param unknown $entry
+     * @param unknown $department
+     * @param \Zend\Filter\Digits $filter \Zend\Filter\Digits
+     * @return string
+     */
+    protected function buildContact($entry, $department, $filter)
+    {
+        $contact = '<div class="panel" itemscope itemtype="http://schema.org/Person">';
+        
+        $contact .= '<div itemscope itemtype="http://schema.org/Organization">';
+        $contact .= '<h5 itemprop="department">';
+        $contact .= $department;
+        $contact .= '</div>';
+        $contact .= '<div class="vcard-description">';
+        
+        $contact .= '<h4 itemprop="name">' . $this->cSalutation($entry->contact->salutation) . $entry->contact->firstName . ' ' . $entry->contact->lastName . '</h4>';
+        $contact .= '<p itemprop="jobTitle">' . $entry->contact->title . '<br />';
+        $contact .= '' . $entry->contact->businessTitle . '</p>';
+        $contact .= '<ul class="vcard-contact-list">';
+        $contact .= '<li>Telefon: <a href="tel:0049' . substr($filter->filter($entry->contact->phoneWork), 1) . '" itemprop="telephone">' . $entry->contact->phoneWork . '</a></li>';
+        if (strlen($entry->contact->phoneFax) > 1) {
+            $contact .= '<li>Fax: <a href="tel:0049' . substr($filter->filter($entry->contact->phoneFax), 1) . '" itemprop="faxNumber">' . $entry->contact->phoneFax . '</a></li>';
+        }
+        if (strlen($entry->contact->alternateEmail) > 1) {
+            $contact .= '<li>E-Mail: <a href="mailto:' . $entry->contact->alternateEmail . '" itemprop="email">' . $entry->contact->alternateEmail . '</a></li>';
+        }
+        $contact .= '</ul>';
+        $contact .= $entry->contact->description;
+        $contact .= '</div>';
+        
+        $contact .= '</div>';
+        return $contact;
+    }
+
+    protected function cSalutation($string)
+    {
+        $str = '';
+        if (strlen($string) > 1) {
+            if (isset($this->salutation[$string])) {
+                return $this->salutation[$string] . ' ';
+            }
+        }
+        return $str;
     }
 }
