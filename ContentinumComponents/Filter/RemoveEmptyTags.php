0b465d1302527b46626ff1c7fee39748d36c80cb
Mikel1961 <michael.jochum@jochum-mediaservices.de>
2016-06-02 12:49:33 +0200

    New filter to remove empty tags from a text string

---
+++ ContentinumComponents/Filter/RemoveEmptyTags.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * contentinum - accessibility websites
+ *
+ * LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category contentinum components
+ * @package Filter
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ * @copyright Copyright (c) 2009-2013 jochum-mediaservices, Katja Jochum (http://www.jochum-mediaservices.de)
+ * @license http://www.opensource.org/licenses/bsd-license
+ * @since contentinum version 5.5
+ * @link      https://github.com/Mikel1961/contentinum-components
+ * @version   1.0.0
+ */
+namespace ContentinumComponents\Filter;
+
+use Zend\Filter\FilterInterface;
+
+/**
+ * Filter and remove empty tags from a text string
+ * 
+ * @author Michael Jochum, michael.jochum@jochum-mediaservices.de
+ *        
+ */
+class RemoveEmptyTags implements FilterInterface
+{
+
+    /**
+     * PCRE is compiled with UTF-8 and Unicode support
+     *
+     * @var mixed
+     *
+     */
+    protected static $_unicodeEnabled;
+
+    /**
+     * Construct and set default parameters
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        if (null === self::$_unicodeEnabled) {
+            self::$_unicodeEnabled = (@preg_match('/\pL/u', 'a')) ? true : false;
+        }
+    }
+
+    /**
+     * (non-PHPdoc)
+     * 
+     * @see \Zend\Filter\FilterInterface::filter()
+     */
+    public function filter($value, $tags = array('p', 'strong', 'div'))
+    {
+        $p_o_tag = '<(' . implode('|', $tags) . ')(?:\s[^>]*[^\/])?';
+        $p_o_tag_short_tag = '\/>';
+        $p_o_tag_long_tag = '>';
+        $p_empty = '(?:&nbsp;|\x00|\xa0|\s)*';
+        $p_cl_tag = '<\/\\1>';
+        $pattern = $p_o_tag . '(?:' . $p_o_tag_short_tag . '|' . $p_o_tag_long_tag . $p_empty . $p_cl_tag . ')';
+        while ($value != ($val = preg_replace('/' . $pattern . '/iS', '', $value)) && is_string($val)) {
+            $value = $val;
+        }
+        return $value;
+    }
+}
